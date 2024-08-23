389bdd92a007c5a63779917b47b579c819476880
KEROJAM <majorekcs@hotmail.com>
2024-08-23 08:49:11 -0600

    quite comentarios

--- ProjectoFinalMain.py
+++ ProjectoFinalMain.py
@@ -185,22 +185,3 @@ if Menu_principal == "Q" or "q":
     
 
 
-#print("| Stock del los articulos Seleccionados: ",  productlist.hoodieVSC)
-#if Product_Type == "Hoodies" and Hoodies_Color_select() == "Verde ":
-#    print("| Tenemos este stock en verde talla L",
-#          productlist.hoodies["cantidadVL"])
-#if TipoPro == "Hoodies" and colorP == "Verde ":
-#    print("| Tenemos este stock en verde talla M ",
-#          productlist.hoodies["cantidadVM"])
-#if TipoPro == "Hoodies" and colorP == "Verde ":
-#    print("|Tenemos este stock en Verde talla S",
-#          productlist.hoodies["cantidadVS"])
-
-#VentaC = int(input("| Cantidad de productos que quieres: "))
-#print("| En tu carrito hay: ", VentaC, " articulos")
-
-
-# print(productlist.hoodies)
-#ventaV = productlist.hoodies.get("cantidadVM")
-#ventaV = ventaV-VentaC
-#productlist.hoodies.update({"cantidadVM": ventaV})
