18b6e885ebbcdc3ee10adadfe90e1a96b4fa9fb9
Alan <alananduaga07@gmail.com>
2024-08-27 07:06:43 -0600

    aaaaa

--- ProjectoFinalMain.py
+++ ProjectoFinalMain.py
@@ -187,8 +187,6 @@ def Hoodies_Color_Black_Size_select():
             print(no_stock)
         if TallaN in size_options:
             return TallaN
-        else:
-            print("| Ese no es un Producto\n| (Verifica si el nombre esta Bien escrito)")
 
 
 def shopping_cart():
