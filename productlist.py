9f39eb08961392b9a1f8f0ac0ac5d19d64975a8d
KEROJAM <majorekcs@hotmail.com>
2024-08-31 20:11:08 -0600

    a lot of comments

--- productlist.py
+++ productlist.py
@@ -1,80 +1,147 @@
 
 hoodies = {
+# Definimos el diccionario Hoodies para organisar mas facil el inventario, color y el precio
     "Precio":  600,
+    # Aqui definimos el precio del producto que es mas facil de manipular
     "colorV": "Verde",
+    # Esta variable es para el color que se va a usar para el carrito de compras
     "cantidadVS": 10,
+    # Esta variable es para definir la cantidad de productos que hay por talla en el color verde
     "cantidadVM": 10,
+    # En esta variable se define la cantidad de productos que tenemos en la talla M en el color verde
     "cantidadVL": 10,
+    # Esta es la cantidad de productos que hay en la talla L con el color verde y se puede usar con la variable
 
     "colorN": "Negro",
+    # Esta es la variable para el color Negro de hoodies para usarse en el carrito de compras
     "cantidadNS": 10,
+    # Esta es la cantidad de productos que tenemos en la talla S y en color Negro
     "cantidadNM": 0,
+    # Estos son los productos que tenemos en talla M y en color Negro
     "cantidadNL": 10,
+    # Esta es la cantidad de hoodies que tenemos en talla L y en color Negro
 
     "colorB": "Blanco",
+    # Este es el color de hoodies en Negro
     "cantidadBS": 10,
+    # Esta es la cantidad de Hoodies en talla S y color Negro
     "cantidadBM": 0,
+    # Esta es la cantidad de Hoodies en talla M y color Negro
     "cantidadBL": 10
+    # Esta es la cantidad de Hoodies en talla L y color Negro
 }
+# Este es el final del diccionario para cerrarlo
 
 camisetas = {
+    # Este es el Diccionario de camisetas para guardar tallas colores y precios
     "Precio": 350,
+    # Este es el precio de camisetas declarado para usarlo con mas facilidad
     "colorA": "Azul",
+    # Esta es la variable de camisetas de color Azul
     "cantidadAS": 10,
+    # Esta es la cantidad de camisetas que tenemos en color Azul y en talla S
     "cantidadAM": 10,
+    # Esta es la cantidad de camisetas que tenemos en color Azul y en talla M
     "cantidadAL": 10,
+    # Esta es la cantidad de camisetas que hay en color Azul y en talla L
 
     "colorN": "Negro",
+    # Esta es la variable que tenemos para las camisas Negras
     "cantidadNS": 10,
+    # Esta es la cantidad de camisetas en color negro y cuantas tenemos en Talla S
     "cantidadNM": 10,
+    # Esta es el numero de camisetas que tenemos en color negro y Talla M
     "cantidadNL": 10,
+    # Esta es la cantidad de camisetas que tenemos en Talla L y en color Negro
 
     "colorB": "Blanco",
+    # Esta es la variable para el Color blanco en camisetas
     "cantidadBS": 10,
+    # Es la cantidad de Camisetas en color Blanco y en talla S que tenemos
     "cantidadBM": 10,
+    # Esta es la cantidad de Camisetas en color Blanco y en talla M que hay en el inventario
     "cantidadBL": 10
+    # Este es el numero de Camisetas en color Blanco y en Talla L que hay en el inventario
 }
+# Aqui se cierra el diccionario para ya no agregar mas valores a las camisetas
 
 Jeans = {
+    # Este es el inico del Diccionario de Jeans, su precio, color y talla
     "Precio": 360,
-    "colorV": "Azul",
-    "cantidadVS": 10,
-    "cantidadVM": 10,
-    "cantidadVL": 10,
+    # Este es el precio de los Jeans
+    "colorA": "Azul",
+    # Ese es uno de los colores que hay en el inventario
+    "cantidadAS": 10,
+    # La cantidad de Jeans que tenemos en color Azul y talla S
+    "cantidadAM": 10,
+    # Esta es la cantidad de Jeans que tenemos en Azul y en talla M
+    "cantidadAL": 10,
+    # Este el numero de jeans que tenemos en azul y en Talla L
 
     "colorN": "Negro",
+    # Este es el otro color de jeans que hay en el inventario
     "cantidadNS": 10,
+    # Esta es la cantidad que hay en Negro y en talla S
     "cantidadNM": 10,
+    # Este es el numero de Jeans que hay en Negro y en Talla M
     "cantidadNL": 10,
+    # Esta es la cantidad de Jeans que hay en Negro y en talla L
 }
+# Este es el final del diccionario de Jeans para ya no introducir mas elementos
 
 Calcetines = {
+    # El inicio del diccionario de calcetines para poner el precio, color y talla
     "Precio": 40,
+    # Este es el precio de los calcetines en una manera accesible
     "colorN": "Negro",
+    # El primer color que hay en Calcentines para usaro en el carrito de compras
     "cantidadNS": 10,
+    # La cantidad de calcentines que hay en Negro y en talla S
     "cantidadNM": 10,
+    # El numero de Calcentines que hay en Negro y en talla M
     "cantidadNL": 10,
+    # la Cantidad de calcentines que hay en negro y en talla L
 
     "colorB": "Blanco",
+    # El segundo color que hay para calcentines
     "cantidadBS": 10,
+    # La cantidad de productos que hay en Blanco y en talla S para calcetines
     "cantidadBM": 10,
+    # La cantidad de Calcetines que tenemos en color Blanco y en talla M
     "cantidadBL": 10
+    # Cantidad de calcentines que hay en el inventario en Blanco y en talla L
 }
+# Aqui se termina el diccionario de calcetines para ya no introducir mas datos y no generar errores
 
-tenis = {
+Tenis = {
+    # Este es el inico del diccionaro de tenis para poder accesar los detalles como color, talla y percio mas facil
     "Precio": 400,
+    # Este es el precio de los tenis puesto de una manera mas accesible
     "colorBN": "Blanco/Negro",
+    # Este es la primera opcion de colores que hay en el inventario
     "cantidadBNS": 10,
+    # la cantidad de tenis que tenemos en Blanco y negro en la talla S
     "cantidadBNM": 10,
+    # Esta es la cantidad se tenis en color Blanco y Negro en la talla M
     "cantidadBNL": 10,
+    # Esta es la cantida de tenis en blacno y negro en la talla L
 
     "colorN": "Negro",
+    # Este es la segunda opcion de color para los tenis
     "cantidadNS": 10,
+    # Esta es la cantidad de tenis que tenemos en inventario para el color negro en la talla S
     "cantidadNM": 10,
+    # Este es el numero de tenis que hay en el inventario para el color Negro en talla M
     "cantidadNL": 10,
+    # Esta es la cantidad de tenis que tenemos en el inventario en color negro y en la talla L
 
     "colorB": "Blanco",
+    # Este es el ultimo color que tenemos para tenis
     "cantidadBS": 10,
+    # Esta es la cantidad de Tenis en color blanco y en talla S
     "cantidadBM": 10,
+    # Este es el numero de tenis en color Blanco y en talla M
     "cantidadBL": 10
+    # Es la cantidad de tenis que tenemos para tenis en color blanco en talla L
 }
+# Y Este es el final del diccionario para tenis
